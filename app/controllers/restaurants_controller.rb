f4e0fac42fb20ad4b23a541c7e0becde8ceefa2c
Edward Schults <edward.schults@gmail.com>
2016-11-25 09:35:51 +0100

    review creation without ajax

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,5 +7,6 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :create ]
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
